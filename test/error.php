c614baafaa904dda2704a73c18813d5c8d5e1aa9
Zordius Chen <zordius@yahoo-inc.com>
2014-02-14 14:09:33 +0800

    add test for error case

--- test/error.php
+++ test/error.php
@@ -1,12 +1,14 @@
 <?php
 require('src/lightncandy.inc');
 $template = <<<VAREND
+This is {{win[ner.test}} ....
+
 <ul>
 {{#each item}}<li>{{name}}</li>
 </ul>
 VAREND
 ;
-$php = LightnCandy::compile($template, Array('flags' => LightnCandy::FLAG_ERROR_LOG | LightnCandy::FLAG_STANDALONE));
+$php = LightnCandy::compile($template, Array('flags' => LightnCandy::FLAG_ERROR_LOG | LightnCandy::FLAG_STANDALONE | LightnCandy::FLAG_HANDLEBARSJS));
 
 echo "Template is:\n$template\n\n";
 echo "Rendered PHP code is:\n$php\n\n";
