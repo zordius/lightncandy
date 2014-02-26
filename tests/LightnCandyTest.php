d0bbe718f0c48077246101df91f7b3f430ba7045
Zordius Chen <zordius@yahoo-inc.com>
2014-02-26 16:20:09 +0800

    update scrutinizer config. add more @expect

--- tests/LightnCandyTest.php
+++ tests/LightnCandyTest.php
@@ -6,6 +6,17 @@ include('src/lightncandy.inc');
 class LightnCandyTest extends PHPUnit_Framework_TestCase
 {
     /**
+     * @covers LightnCandy::_fileext
+     */
+    public function testOn__fileext() {
+        $method = new ReflectionMethod('LightnCandy', '_fileext');
+        $method->setAccessible(true);
+        $this->assertEquals(Array('.tmpl'), $method->invoke(null, Array()));
+        $this->assertEquals(Array('test'), $method->invoke(null, Array('fileext' => 'test')));
+        $this->assertEquals(Array('test1'), $method->invoke(null, Array('fileext' => Array('test1'))));
+        $this->assertEquals(Array('test2', 'test3'), $method->invoke(null, Array('fileext' => Array('test2', 'test3'))));
+    }
+    /**
      * @covers LightnCandy::_scope
      */
     public function testOn__scope() {
