3dca233b4895386240a2e464a48da76750d094f3
Zordius Chen <zordius@yahoo-inc.com>
2014-06-05 10:34:13 +0800

    add tests for #45

---
+++ tests/issue45.php
@@ -0,0 +1,23 @@
+<?php
+require('src/lightncandy.php');
+
+$template = '{{{a.b.c}}}, {{a.b.bar}}';
+
+class foo {
+    function bar() {
+        return 'OK!'; 
+    }
+}
+
+$php = LightnCandy::compile($template, Array(
+    'flags' => LightnCandy::FLAG_ERROR_LOG | 0 * LightnCandy::FLAG_STANDALONE | LightnCandy::FLAG_HANDLEBARSJS,
+    )
+);
+
+$renderer = LightnCandy::prepare($php);
+
+echo "Render result:\n";
+echo $renderer(Array('a' => Array('b' => new foo)));
+echo "\n";
+
+?>
