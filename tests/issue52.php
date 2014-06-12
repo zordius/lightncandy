74124ef01ae8007e886fc086cbf34c8822a5ef7f
Zordius Chen <zordius@yahoo-inc.com>
2014-06-12 09:40:53 +0800

    add test for issue #52

---
+++ tests/issue52.php
@@ -0,0 +1,25 @@
+<?php
+require('src/lightncandy.php');
+$template = '{{{test tmp}}} should be happy!';
+VAREND
+;
+
+$php = LightnCandy::compile($template, array(
+    'flags' => LightnCandy::FLAG_HANDLEBARSJS | LightnCandy::FLAG_ERROR_EXCEPTION,
+    'helpers' => array(
+        'test'
+    )
+));
+
+function test ($array) {
+   return is_array($array) ? 'IS_ARRAY' : 'NOT_ARRAY';
+}
+
+echo "Rendered PHP code is:\n$php\n\n";
+$renderer = LightnCandy::prepare($php);
+
+echo "Render result:\n";
+echo $renderer(Array('tmp' => Array('A', 'B', 'C')));
+echo "\n";
+
+?>
