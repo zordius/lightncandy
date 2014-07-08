e6b923ff64d36a3394bd5b7870f7e82ed69afcb6
Zordius Chen <zordius@yahoo-inc.com>
2014-07-08 09:23:15 +0800

    add tests for #68

---
+++ tests/issue68.php
@@ -0,0 +1,29 @@
+<?php
+require('src/lightncandy.php');
+$template = '{{#myeach foo}} Test! {{this}} {{/myeach}}';
+VAREND
+;
+
+$php = LightnCandy::compile($template, array(
+    'flags' => LightnCandy::FLAG_HANDLEBARSJS | LightnCandy::FLAG_ERROR_EXCEPTION,
+    'hbhelpers' => array(
+        'myeach'
+    )
+));
+
+function myeach($context, $options) {
+    $ret = '';
+    foreach ($context as $cx) {
+        $ret .= $options['fn']($cx);
+    }
+    return $ret;
+}
+
+echo "Rendered PHP code is:\n$php\n\n";
+$renderer = LightnCandy::prepare($php);
+
+echo "Render result:\n";
+echo $renderer(Array('foo' => Array('A', 'B', 'bar' => Array('C', 'D', 'E'))));
+echo "\n";
+
+?>
