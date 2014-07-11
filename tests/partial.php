f31a5294a2b9321156657fb47c4d198f9ede3008
Zordius Chen <zordius@yahoo-inc.com>
2014-07-11 09:43:48 +0800

    test on recursive

--- tests/partial.php
+++ tests/partial.php
@@ -1,6 +1,6 @@
 <?php
 require('src/lightncandy.php');
-$template = '{{#each foo}} Test! {{this}} {{/each}}{{> test1}} !';
+$template = '{{#each foo}} Test! {{this}} {{/each}}{{> test1}} ! >>> {{>recursive}}';
 VAREND
 ;
 
@@ -12,8 +12,27 @@ $php = LightnCandy::compile($template, array(
 echo "Rendered PHP code is:\n$php\n\n";
 $renderer = LightnCandy::prepare($php);
 
-echo "Render result:\n";
+echo "Render result 1:\n";
 echo $renderer(Array('foo' => Array('A', 'B', 'bar' => Array('C', 'D', 'E'))));
+echo "Render result 2:\n";
+echo $renderer(Array(
+ 'bar' => 1,
+ 'foo' => Array(
+  'bar' => 3,
+  'foo' => Array(
+   'bar' => 5,
+   'foo' => Array(
+    'bar' => 7,
+    'foo' => Array(
+     'bar' => 11,
+     'foo' => Array(
+      'no foo here'
+     )
+    )
+   )
+  )
+ )
+));
 echo "\n";
 
 ?>
