8f741d6a5fc5b6d33f8fee9fbb61a9046b98722a
Zordius Chen <zordius@yahoo-inc.com>
2014-07-15 09:23:02 +0800

    move issue44 into regression

--- tests/regressionTest.php
+++ tests/regressionTest.php
@@ -28,7 +28,23 @@ class regressionTest extends PHPUnit_Framework_TestCase
                 'options' => null,
                 'data' => Array('tt' => 'bla bla bla'),
                 'expected' => 'bla bla bla'
-            )
+            ),
+
+            Array(
+                'id' => 44,
+                'template' => '<div class="terms-text"> {{render "artists-terms"}} </div>',
+                'options' => Array(
+                    'flags' => LightnCandy::FLAG_HANDLEBARSJS | LightnCandy::FLAG_ERROR_LOG | LightnCandy::FLAG_EXTHELPER,
+                    'helpers' => Array(
+                        'url',
+                        'render' => function($view,$data = array()) {
+                            return 'OK!';
+                         }
+                    )
+                ),
+                'data' => Array('tt' => 'bla bla bla'),
+                'expected' => '<div class="terms-text"> OK! </div>'
+            ),
         );
 
         return $issues;
