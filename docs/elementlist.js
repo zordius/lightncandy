5dc9e2532254320a9f105dd739c754db6d874dcd
GithubAction <zordius@users.noreply.gihub.com>
2021-07-11 01:35:18 +0000

    chore: auto generated documents from branch master @48d0e6021b880463c281a16a783299800ab17c48 [ci skip]

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","LightnCandy\\Compiler"],["c","LightnCandy\\Context"],["c","LightnCandy\\Encoder"],["c","LightnCandy\\Exporter"],["c","LightnCandy\\Expression"],["c","LightnCandy\\Flags"],["c","LightnCandy\\LightnCandy"],["c","LightnCandy\\Parser"],["c","LightnCandy\\Partial"],["c","LightnCandy\\Runtime"],["c","LightnCandy\\SafeString"],["c","LightnCandy\\StringObject"],["c","LightnCandy\\Token"],["c","LightnCandy\\Validator"]];
